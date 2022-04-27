01cdcb4c0d10d92d05ded15c5d6721bd57d23875
Uzair <muhammaduzairbaloch52@gmail.com>
2022-04-27 12:25:07 +0500

    Add

--- src/Components/BookingHistoryCard.js
+++ src/Components/BookingHistoryCard.js
@@ -7,36 +7,43 @@ const BookingHistoryCard = () => {
     <View
       style={{
         marginHorizontal: 30,
-        marginVertical: 20,
-        borderWidth: 1,
+        marginVertical: 10,
+        borderBottomWidth: 1,
         borderBottomColor: '#DDDDDD',
+        paddingBottom: 20,
       }}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <View style={{marginTop: 10}}>
           <Text style={{color: '#000', fontSize: 22, fontWeight: '600'}}>
             Dr Zakaria
           </Text>
+
+          <Text style={{color: 'black'}}>Rating</Text>
         </View>
-        <View style={{}}>
+        <View>
           <Image
             style={{width: 100, height: 100}}
             source={require('../assets/images/Dr1.jpg')}
           />
         </View>
       </View>
+
       <View>
         <View style={{flexDirection: 'row'}}>
-          <Ionicons name="navigate" size={18} color="#000" />
-          <Text style={{color: '#000'}}>Cardiologist 106 Medlicott rd</Text>
+          <View style={{marginLeft: 5, paddingBottom: 10}}>
+            <Ionicons name="navigate" size={18} color="#000" />
+          </View>
+          <View style={{marginLeft: 10}}>
+            <Text style={{color: 'grey'}}>Cardiologist 106 Medlicott rd</Text>
+          </View>
         </View>
         <View style={{flexDirection: 'row'}}>
-          <Ionicons
-            name="time-outline"
-            color="#000"
-            size={22}
-            style={{marginRight: 5}}
-          />
-          <Text style={{color: '#000'}}>Open Until 20:00PM</Text>
+          <View style={{marginLeft: 5}}>
+            <Ionicons name="time-outline" color="#000" size={22} />
+          </View>
+          <View style={{marginLeft: 10}}>
+            <Text style={{color: 'grey'}}>Open Until 20:00PM</Text>
+          </View>
         </View>
       </View>
     </View>
